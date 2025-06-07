300eefe138532080b7a8e7dc874fc68d7c267b73
Gedeon FREYCINET <gedeon.freycinet@wfp.org>
2025-06-06 20:40:15 -0400

    Tout tès yo pase ankò

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
+
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
